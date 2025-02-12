2fb5885b557388817c22d38764b5b8e7359dec9f
OsamaNagi <osamanagiworks@gmail.com>
2025-02-12 14:39:47 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OsamaNagi/chirpy
+
+go 1.23.6
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	server := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fmt.Printf("Server listening on port %s\n", port)
+	log.Fatal(server.ListenAndServe())
+}
