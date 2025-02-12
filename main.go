fc027a8fb0416c7605c5b683d8975b868cf52591
OsamaNagi <osamanagiworks@gmail.com>
2025-02-12 14:57:37 +0200

    WIP

--- main.go
+++ main.go
@@ -8,14 +8,23 @@ import (
 
 func main() {
 	const port = "8080"
+	const ROOT = "."
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(ROOT))))
+	mux.HandleFunc("/healthz", handlerReadiness)
+
 	server := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
 	fmt.Printf("Server listening on port %s\n", port)
 	log.Fatal(server.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
